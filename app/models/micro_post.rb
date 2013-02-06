488551b29fc0bb560448344ef14551c9558e54da
Michael Kaplan <kaplanm99@gmail.com>
2013-02-06 19:47:15 +0000

    MicroPost model

---
+++ app/models/micro_post.rb
@@ -0,0 +1,6 @@
+class MicroPost < ActiveRecord::Base
+  attr_accessible :content, :user_id
+  validates :user_id, presence: true
+  validates :content, presence: true,
+					  length: {minimum: 4, maximum: 140 }
+end
